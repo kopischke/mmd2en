b122a1e21427fea9218147fcc0e6aa25bf01093f
Martin Kopischke <martin@kopischke.net>
2013-12-13 15:43:16 +0100

    Build system: dynamical data generators in MustacheTask
    Allowing Proc / Lambda @data should enable dynamic data generation
    without resorting to before / after hook hacks.

--- ext/rake/mustache_task.rb
+++ ext/rake/mustache_task.rb
@@ -12,15 +12,15 @@ module Rake
     alias :template  :base
     alias :template= :base=
 
-    # restrict `actions` and `args` access to r/o
-    protected :action=, :args=
+    # restrict `actions` access to r/o
+    protected :action=
 
     def initialize(target, template_file: nil, data: nil, **kwargs, &block)
       @data  = data
       action = ->(*_){
         puts "Rendering Mustache template '#{@base.pathmap('%f')}' to '#{@target.pathmap('%f')}'..."
         Mustache.template_file = @base
-        File.write(@target, Mustache.render(@data))
+        File.write(@target, Mustache.render(@data.respond_to?(:call) ? @data.call(*@args) : @data))
       }
       template_file = template_file || "#{target}.mustache"
       super(target, template_file, action, **kwargs, &block)
