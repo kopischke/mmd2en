b876f0fd9bd9831327016dfae7deb0f4d54aae6c
Martin Kopischke <martin@kopischke.net>
2013-12-19 17:51:34 +0100

    Build system: refine ZipTask dependency management
    Defaulting to the files to be zipped as dependencies is fine, but we
    should not clobber the option to add other (possibly named) tasks to
    the requirements.

--- ext/rake/zip_task.rb
+++ ext/rake/zip_task.rb
@@ -5,25 +5,24 @@ module Rake
   # Rake task to generate ZIP archives wrapping the `zip` utility
   # (because `zip`, and hence PackageTask, are painful to configure).
   class ZipTask < SmartFileTask
-    attr_accessor :move, :recurse, :relative, :exclude
+    attr_accessor :files, :move, :recurse, :relative, :exclude
 
     # syntactic sugar
     alias :archive  :target
     alias :archive= :target=
-    alias :files    :base
-    alias :files=   :base=
 
     # restrict `actions` access to r/o
     protected :action=
 
     def initialize(archive_file, file_paths = nil, move: false, recurse: true, relative: true, exclude: [], **kwargs, &block)
+      @files    = file_paths
       @move     = move
       @recurse  = recurse
       @relative = relative
       @exclude  = exclude
       action = ->(*_){
         archive_path = "#{@target.shellescape.sub(/\.zip$/i, '')}.zip"
-        @base.sort.each do |path|
+        @files.sort.each do |path|
           zip_command = [].tap {|cmd|
             cmd << "cd #{File.dirname(path).shellescape};" if @relative
             cmd << 'zip'
@@ -38,7 +37,7 @@ module Rake
           fail "Error generating package archive '#{@target}': `zip` returned #{$?.exitstatus}" unless $?.exitstatus == 0
         end
       }
-      super(archive_file, file_paths, action, **kwargs, &block)
+      super(archive_file, @files, action, **kwargs, &block)
     end
   end
 end
