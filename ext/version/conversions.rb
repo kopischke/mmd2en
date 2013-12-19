3c5988cb50dd0bf9f0dd3850871a8258a664f9f0
Martin Kopischke <martin@kopischke.net>
2013-12-19 02:27:32 +0100

    Build system: Version gem compatible release candidates
    If it wasn’t for the Rake tasks, things would be easier using
    Versionify.

--- ext/version/conversions.rb
+++ ext/version/conversions.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 class Version
   def to_friendly
-    names = {'a' => 'alpha', 'b' => 'beta', 'rc' => 'release candidate'}
+    names = {'a' => 'alpha', 'b' => 'beta', 'r' => 'release candidate'}
     match = String(self).match(/^(.+)(#{names.keys.join('|')})([[:digit:]]+)?$/i)
     match ? "#{match[1]} #{names[match[2]]} #{match[3]}" : self.to_s
   end
