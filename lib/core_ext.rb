14feb26189bd2a2bcd58b89f8855e4030c8d2f5e
Martin Kopischke <martin@kopischke.net>
2013-12-27 23:12:09 +0100

    IO.dump / .dump! opt handling
    Allows passing of encoding options, among others.

--- lib/core_ext.rb
+++ lib/core_ext.rb
@@ -4,14 +4,14 @@ module ARGFParser
   # Returns an Array of all files passed, plus STDIN, as Files open for reading.
   # Does not block on empty STDIN and no files as it uses IO.dump.
   # Hat tip Onteria, http://rubyit.wordpress.com/2011/07/25/ruby-and-argf/
-  def to_files
+  def to_files(**options)
     files = []
     until ARGV.empty?   # files passed on command line
-      files << File.new(ARGV[0]) rescue nil
+      files << File.new(ARGV[0], 'r', **options) rescue nil
       ARGV.shift
     end
     if self.path == '-' # STDIN
-      file = self.file.dump
+      file = self.file.dump(**options)
       files << File.new(file.path) unless file.nil?
     end
     files.compact
@@ -23,10 +23,10 @@ ARGF.extend ARGFParser
 class IO
   # Dump IO into a Tempfile (closed on return), without blocking.
   # Hat tip Eric Lubow, http://eric.lubow.org/2010/ruby/multiple-input-locations-from-bash-into-ruby/
-  def dump
+  def dump(**options)
     return nil if self.closed?
-    Tempfile.open('IO-dump') do |temp|
       temp.write(self.read_nonblock(1)) # raises IO::WaitReadable if IO is blocking
+    Tempfile.open('IO-dump', **options) do |temp|
       self.each_line do |line| temp.write(line) end
       temp
     end
@@ -35,8 +35,8 @@ class IO
   end
 
   # Like `dump`, but closes the IO stream after dumping.
-  def dump!
-    temp = self.dump and self.close
+  def dump!(**options)
+    temp = self.dump(**options) and self.close
     temp
   end
 end
