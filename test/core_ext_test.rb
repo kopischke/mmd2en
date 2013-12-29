693a85904c7066af7285479e66477bef9b39d061
Martin Kopischke <martin@kopischke.net>
2013-12-30 00:54:25 +0100

    Update Core Extensions unit tests
    – test for non-blocking option in `IO.eof`
    – test for `IO.guess_external_encoding`
    – test for option passing in `IO.dump` / `IO.dump!`
    – general test suite cleanup

--- test/core_ext_test.rb
+++ test/core_ext_test.rb
@@ -1,62 +1,66 @@
 # encoding: UTF-8
 require_relative 'test_helper'
 require 'core_ext'
+require "tempfile"
+
+def ruby_run(cmd, *args)
+  %x{ruby -I #{File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))} -e 'require "core_ext"; #{cmd}' #{args.join(' ')}}
+end
 
 class TestARGF < Minitest::Test
-  def setup
-    @lib     = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
-    @require = 'require "core_ext"'
-  end
-
   def test_exposes_to_files_method
     assert_respond_to ARGF, :to_files
   end
 
-  def test_returns_passed_files_and_stdin
+  def test_to_files_returns_passed_files_and_stdin
     argf_files = [__FILE__, __FILE__]
     stdin_file = __FILE__
 
-    out_files = %x{ruby -I #{@lib} -e '#{@require}; puts ARGF.to_files' #{argf_files.join(' ')}}.chomp.split($/)
+    out_files = ruby_run('puts ARGF.to_files', *argf_files).chomp.split($/)
     assert_equal argf_files.count, out_files.count
     assert out_files.all? {|f| f.match(/#<File:(.+)>/) }, "Output does not entirely consist of File objects."
 
-    argf_in = argf_files.map {|f| File.read(f) }.join
-    output  = %x{ruby -I #{@lib} -e '#{@require}; ARGF.to_files.each {|f| puts File.read(f.path) }' #{argf_files.join(' ')}}
+    argf_in   = argf_files.map {|f| File.read(f) }.join
+    output    = ruby_run('ARGF.to_files.each {|f| puts File.read(f.path) }', *argf_files)
     assert_equal argf_in, output
 
-    out_files = %x{ruby -I #{@lib} -e '#{@require}; puts ARGF.to_files' < #{stdin_file}}.chomp.split($/)
+    out_files = ruby_run('puts ARGF.to_files', "< #{stdin_file}").chomp.split($/)
     assert_equal 1, out_files.count
     assert out_files.all? {|f| f.match(/#<File:(.+)>/) }, "Output does not entirely consist of File objects."
 
-    stdin_in = File.read(stdin_file)
-    output   = %x{ruby -I #{@lib} -e '#{@require}; ARGF.to_files.each {|f| puts File.read(f.path) }' < #{stdin_file}}
+    stdin_in  = File.read(stdin_file)
+    output    = ruby_run('ARGF.to_files.each {|f| puts File.read(f.path) }', '<', stdin_file)
     assert_equal stdin_in, output
 
-    out_files = %x{ruby -I #{@lib} -e '#{@require}; puts ARGF.to_files' #{argf_files.join(' ')} < #{stdin_file}}.chomp.split($/)
+    out_files = ruby_run('puts ARGF.to_files', *argf_files, '<', stdin_file).chomp.split($/)
     assert_equal argf_files.count+1, out_files.count
     assert out_files.all? {|f| f.match(/#<File:(.+)>/) }, "Output does not entirely consist of File objects."
 
-    output = %x{ruby -I #{@lib} -e '#{@require}; ARGF.to_files.each {|f| puts File.read(f.path) }' #{argf_files.join(' ')} < #{stdin_file}}
+    output    = ruby_run('ARGF.to_files.each {|f| puts File.read(f.path) }', *argf_files, '<', stdin_file)
     assert_equal argf_in << stdin_in, output
   end
 
-  def test_rejects_invalid_files
+  def test_to_files_rejects_invalid_files
     argf_files = ['/dev/null/nada/limbo', __FILE__]
-    out_files = %x{ruby -I #{@lib} -e '#{@require}; puts ARGF.to_files' #{argf_files.join(' ')}}.chomp.split($/)
+    out_files  = ruby_run('puts ARGF.to_files', *argf_files).chomp.split($/)
     assert_equal argf_files.select {|f| File.readable?(f) }.count, out_files.count
   end
 
-  def test_does_not_block_on_empty_stdin
-    out_files = %x{ruby -I #{@lib} -e '#{@require}; puts ARGF.to_files'}.chomp.split($/)
+  def test_to_files_does_not_block_on_empty_stdin
+    out_files = ruby_run('puts ARGF.to_files').chomp.split($/)
     assert_equal 0, out_files.count
   end
 end
 
 class TestIO < Minitest::Test
   def setup
+    @r, @w = IO.pipe
     @content = "Some output\non two lines"
-    @lib     = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
-    @require = 'require "core_ext"'
+  end
+
+  def teardown
+    @r.close unless @r.closed?
+    @w.close unless @w.closed?
   end
 
   def test_exposes_dump_methods
@@ -64,23 +68,68 @@ class TestIO < Minitest::Test
     assert_respond_to STDIN, :dump!
   end
 
-  def test_dumps_contents_to_file_without_closing
-    dumped = %x{echo "#{@content}" | ruby -I #{@lib} -e '#{@require}; f = ARGF.file.dump; puts File.read(f.path)' }.chomp
-    assert_equal @content, dumped
-    open   = %x{echo "#{@content}" | ruby -I #{@lib} -e '#{@require}; f = ARGF.file.dump; puts ARGF.file.closed?' }.chomp
-    assert_equal String(false), open
+  def test_dump_dumps_contents_to_file_without_closing
+    Thread.new { @w.puts @content; @w.close }
+    assert_equal @content, File.read(@r.dump.path).chomp
+    assert_equal false, @r.closed?
   end
 
-  def test_dumps_contents_to_file_and_closes_stream
-    dumped = %x{echo "#{@content}" | ruby -I #{@lib} -e '#{@require}; f = ARGF.file.dump!; puts File.read(f.path)' }.chomp
-    assert_equal @content, dumped
-    open   = %x{echo "#{@content}" | ruby -I #{@lib} -e '#{@require}; f = ARGF.file.dump!; puts ARGF.file.closed?' }.chomp
-    assert_equal String(true), open
+  def test_dump_bang_dumps_contents_to_file_and_closes_stream
+    Thread.new { @w.puts @content; @w.close }
+    assert_equal @content, File.read(@r.dump!.path).chomp
+    assert_equal true, @r.closed?
   end
 
-  def test_does_not_block_on_empty_stream
-    output = "And we have: "
-    dumped = %x{ruby -I lib -e '#{@require}; f = ARGF.file.dump; puts "#{output}" << String(f)' }.chomp
-    assert_equal output, dumped
+  def test_dump_passes_options_to_tempfile
+    kwargs   = {external_encoding: Encoding::UTF_16LE}
+    tempfile = Minitest::Mock.new
+    tempfile.expect :open, Tempfile.new('TEST'), [String, kwargs]
+    Thread.new { @w.puts @content; @w.close }
+    IO.stub_const(:Tempfile, tempfile) do
+      @r.dump!(**kwargs)
+      tempfile.verify
+    end
+  end
+
+  def test_read_blocking_returns_true_on_blocking_io
+    require 'time'
+    start_time  = Time.now
+    sleep_delay = 30
+    Thread.new { sleep sleep_delay; @w.close }
+    assert_equal    true, @r.read_blocking?
+    assert_operator Time.now - start_time, :<, sleep_delay
+  end
+
+  def test_read_blocking_returns_false_on_non_blocking_io
+    file = File.new(__FILE__)
+    assert_equal false, file.read_blocking?
+  rescue => e
+    file.close
+    raise e
+  end
+
+  def test_exposes_real_encoding_method
+    assert_respond_to STDIN, :real_encoding
+  end
+
+  def test_passes_real_encoding_request_to_file
+    test_path = File.join(File.dirname(__FILE__), 'content', 'Test-utf16le.mmd')
+    test_file = File.new(test_path)
+    assert_equal test_file.real_encoding, File.real_encoding(test_path)
+  rescue => e
+    test_file and test_file.close
+    raise e
+  end
+end
+
+class TestFile < Minitest::Test
+  def test_real_encoding_recognizes_encodings
+    test_file = File.join(File.dirname(__FILE__), 'content', 'Test-utf16le.mmd')
+    assert_equal Encoding::UTF_16LE, File.real_encoding(test_file)
+  end
+
+  def test_real_encoding_returns_nil_when_unsure
+    test_file = File.join(File.dirname(__FILE__), 'content', 'Test-macroman.mmd')
+    assert_nil File.real_encoding(test_file)
   end
 end
