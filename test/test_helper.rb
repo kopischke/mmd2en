03945e40c31f97a89391a8604125d84d4afd2dc3
Martin Kopischke <martin@kopischke.net>
2014-01-12 04:31:09 +0100

    Whitespace cleanup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require 'minitest/mock'
 require 'minitest/stub_const'
 
 begin # Minitest reporters are entirely optional
-	require 'minitest/reporters'
-	Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+  require 'minitest/reporters'
+  Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 rescue LoadError
 end
