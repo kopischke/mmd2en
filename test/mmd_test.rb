40d0a6a1953d4bd4d4fe6311d0385b1aefba8cab
Martin Kopischke <martin@kopischke.net>
2014-01-16 01:48:33 +0100

    MultiMarkdownParser unit tests rewrite
    Adapt to modified lib, better coverage, use modules for Stubs and
    Mocks, only mock the ShellRunner, less magic mock syntax, wrap the
    #stub_const calls.

--- test/mmd_test.rb
+++ test/mmd_test.rb
@@ -2,193 +2,234 @@
 require_relative 'test_helper'
 require 'mmd'
 
-# Mocks
-class MockEnv < Minitest::Mock
-  def initialize(value = nil)
-    super()
-    2.times do expect :[], value, ['MULTIMARKDOWN'] end
+module Mocks
+  class ShellRunner < Minitest::Mock
+    # Note: all `(using_)?bin`arguments expect a Stub::Bin
+    def initialize
+      super()
+      expect :new, self
+    end
+
+    def expect_shell_query_returning(*bins)
+      command = 'echo $MULTIMARKDOWN; which -a multimarkdown'
+      expect :run_command, bins.map(&:path).join($/), ['bash', '-lc', command]
+    end
+
+    def expect_version_query_for(*bins)
+      bins.select {|b| b.executable }.each do |b|
+        expect :run_command, "MultiMarkdown version #{b.version}", [b.path, '-v']
+      end
+    end
+
+    def expect_conversion_command_for(file, using_bin, to_format: :html, to_file: false)
+      options = ['-t', to_format.to_s]
+      options.push('-o', to_file) if to_file
+      expect :run_command, '', [using_bin.path, *options, file]
+    end
+
+    def expect_metadata_listing_query_for(file, using_bin, *return_keys)
+      expect :run_command, return_keys.join($/), [using_bin.path, '-m', file]
+    end
+
+    def expect_metadata_key_query_for(key, file, using_bin, returns)
+      expect :run_command, returns, [using_bin.path, '-e', key, file]
+    end
   end
 end
 
-class MockShellRunner < Minitest::Mock
-  def initialize(path_bins, env_bin = nil)
-    super()
-    expect :new, self
-    expect :run_command, path_bins.map(&:path).join($/), ['bash', '-lc', 'which -a multimarkdown']
-    all_bins = env_bin.nil? ? path_bins : [env_bin] | path_bins
-    all_bins.select {|b| b.executable }.each do |b| expect :run_command, "MultiMarkdown version #{b.version}", [b.path, '-v'] end
+module Stubs
+  # `multimarkdown` binary with metadata
+  Bin = Struct.new(:path, :executable, :version) do
+    def ok?
+     self.executable && self.version >= MultiMarkdownParser::MINIMUM_VERSION
+    end
+  end
+
+  # File.method stubs
+  File = Struct.new(:path) do
+    def self.executable?(path)
+      path.match('/true/')
+    end
+
+    def self.expand_path(path)
+      ::File.expand_path(path)
+    end
+  end
+
+  # stub_const call wrapper for nested stubbed runner and File lib
+  def self.with_runner(runner, &block)
+    MultiMarkdownParser.stub_const(:ShellRunner, runner) do
+      MultiMarkdownParser.stub_const(:File, Stubs::File) do
+        block.call
+      end
+    end
   end
 end
-
-class MockFile < Minitest::Mock
-  def initialize(bins)
-    super()
-    bins.each do |b| expect :executable?, b.executable, [b.path] end
-  end
-end
-
-# Stubs
-Struct.new('Bin', :path, :executable, :version) do
-  def ok?
-    executable && version >= MultiMarkdownParser::MINIMUM_VERSION
-  end
-end
-
-Struct.new('File', :path)
 
 class TestMultiMarkdownParser < Minitest::Test
-  # default setup: ENV unset and at least one compatible binary + one incompatible binary in PATH
   def setup
-    i = 1
-    @bins = [].tap {|a|
-      until a.select {|b| b.ok? }.count >= 1 && a.reject {|b| b.ok? }.count >= 1 do
-        i += rand(9)+1
-        v  = "#{('3.0'..'5.1').to_a.sample}.#{rand(9)}"
-        a << Struct::Bin.new("/rand/#{i}/multimarkdown", [true, false].sample, v)
-      end
-    }
-    @good = @bins.select {|b| b.ok? }
-    @best = @good.find   {|b| b.version = @good.map(&:version).sort.last }
+    @bins = [] # available binaries
+    until @bins.select {|b| b.ok? }.count >= 1 && @bins.reject {|b| b.ok? }.count >= 1 do
+      index ||= 0
+      exec    = [true, false].sample
+      version = "#{('3.0'..'5.1').to_a.sample}.#{rand(9)}"
+      path    = "/stub/#{index += 1}/#{version}/#{exec}/multimarkdown"
+      @bins  << Stubs::Bin.new(path, exec, version)
+    end
 
+    # specific @bins subset and a designated dud
+    @exec = @bins.select {|bin| bin.executable }
+    @good = @bins.select {|bin| bin.ok? }
+    @best = @good.max_by {|bin| bin.version }
+    @dud  = Stubs::Bin.new('', false, '0.0.0')
+
+    # metadata key selection
     @meta_keys = ['title', 'tags', 'notebook', 'author', 'date created', 'permalink']
 
-    @env      = MockEnv.new
-    @runner   = MockShellRunner.new(@bins)
-    @file_lib = MockFile.new(@bins)
+    # file source path
+    @source   = '/stub/file/source.md'
 
-    MultiMarkdownParser.stub_const(:ENV, @env) do
-      MultiMarkdownParser.stub_const(:ShellRunner, @runner) do
-        MultiMarkdownParser.stub_const(:File, @file_lib) do
-          @mmd = MultiMarkdownParser.new
-        end
-      end
+    # blank mock ShellRunner instance
+    @runner   = Mocks::ShellRunner.new
+
+    # plain vanilla MultiMarkdownParser instance
+    runner    = Mocks::ShellRunner.new
+    runner.expect_shell_query_returning(@best)
+    runner.expect_version_query_for(@best)
+    Stubs.with_runner(runner) do
+      @mmd = MultiMarkdownParser.new
     end
-
-    @env.verify
-    @runner.verify
-    @file_lib.verify
   end
 
-  # .new() raises a RuntimeError if no MMD binary is found
-  def test_new_raises_error_if_no_executable_mmd_is_found
-    runner = MockShellRunner.new([Struct::Bin.new('', false, '0.0.0')])
-    MultiMarkdownParser.stub_const(:ShellRunner, runner) do
+  def test_new_completes_using_best_binary_found
+    @runner.expect_shell_query_returning(*@bins)
+    @runner.expect_version_query_for(*@exec)
+    Stubs.with_runner(@runner) do
+      assert_equal @best.path, MultiMarkdownParser.new.bin
+    end
+    @runner.verify
+  end
+
+  def test_new_ignores_empty_env_settings
+    @runner.expect_shell_query_returning(@dud, @best)
+    @runner.expect_version_query_for(@best)
+    Stubs.with_runner(@runner) do
+      assert_equal @best.path, MultiMarkdownParser.new.bin
+    end
+    @runner.verify
+  end
+
+  def test_new_raises_error_if_no_binary_is_found
+    @runner.expect_shell_query_returning(@dud)
+    Stubs.with_runner(@runner) do
       assert_raises(RuntimeError) { MultiMarkdownParser.new }
     end
-    runner.verify
+    @runner.verify
   end
 
-  # else the best MMD binary found in PATH is used
-  def test_new_completes_using_best_executable_found_in_path
-    assert_equal @best.path, @mmd.bin
-  end
-
-  # values in MULTIMARKDOWN are used only if qualifying as best binary
-  def test_new_completes_using_env_setting_if_best
-    [@best, @bins.reject {|b| b.ok? }.sample].each do |env_bin|
-      env      = MockEnv.new(env_bin.path)
-      runner   = MockShellRunner.new(@bins, env_bin)
-      file_lib = MockFile.new([env_bin] | @bins)
-
-      MultiMarkdownParser.stub_const(:ENV, env) do
-        MultiMarkdownParser.stub_const(:ShellRunner, runner) do
-          MultiMarkdownParser.stub_const(:File, file_lib) do
-            assert_equal @best.path, MultiMarkdownParser.new.bin
-          end
-        end
-      end
-
-      env.verify
-      runner.verify
-      file_lib.verify
-    end
-  end
-
-  # String conversions return @bin’s path
   def test_bin_as_string_is_path
     assert_equal @mmd.bin, @mmd.to_path
     assert_equal @mmd.bin, @mmd.to_s
     assert_equal @mmd.bin, @mmd.to_str
   end
 
-  # relevant Pathname properties of @bin are exposed
-  def test_pathname_properties_exposed
+  def test_exposes_bin_pathname_properties
     [
-      :expand_path, :realpath, :realdirpath, :dirname, :basename, :parent,
-      :size, :stat, :atime, :ctime, :owned?, :grpowned?, :symlink?, :readlink
-    ].each {|method| assert_respond_to @mmd, method }
+      :expand_path,
+      :realpath,
+      :realdirpath,
+      :dirname,
+      :basename,
+      :parent,
+      :size,
+      :stat,
+      :atime,
+      :ctime,
+      :owned?,
+      :grpowned?,
+      :symlink?,
+      :readlink
+    ].each do |method| assert_respond_to @mmd, method end
   end
 
-  # version() returns a SemanticVersion
-  def test_correct_semantic_version_retrieved
+  def test_version_returns_correct_semantic_version
     assert_instance_of SemanticVersion, @mmd.version
     assert_equal  SemanticVersion.new(@best.version), @mmd.version.to_s
   end
 
-  # retrieve all metadata in source file if MMD supports the '-m' option
-  def test_loads_all_metadata_if_supported_and_fallback_set_if_not
-    source  = Struct::File.new('/rand/new/source.md')
-    fb_keys = @meta_keys.sample(3)
+  def test_load_file_metadata_queries_all_keys_if_m_supported
+    fallback = @meta_keys.sample(3)
+    keys     = @meta_keys.sample(4)
+    bin      = Stubs::Bin.new(@exec.sample.path, true, MultiMarkdownParser::BASELINE_VERSION)
 
-    [
-      { MultiMarkdownParser::METADATA_VERSION => @meta_keys},
-      {(MultiMarkdownParser::MINIMUM_VERSION...MultiMarkdownParser::METADATA_VERSION).to_a.sample => fb_keys}
-    ].each do |set|
-      version  = set.keys.first
-      keys     = set.values.first
-      bin      = Struct::Bin.new('/rand/use/multimarkdown', true, version)
-      env      = MockEnv.new
-      file_lib = MockFile.new([bin])
+    @runner.expect_shell_query_returning(bin)
+    @runner.expect_version_query_for(bin)
+    @runner.expect_metadata_listing_query_for(@source, bin, *keys)
+    keys.each do |key| @runner.expect_metadata_key_query_for(key, @source, bin, "Value for #{key} metadata.") end
 
-      runner   = MockShellRunner.new([bin]);
-      runner.expect :run_command, keys.join($/), [bin.path, '-m', source.path] if version >= MultiMarkdownParser::METADATA_VERSION
-      keys.each {|k| runner.expect :run_command, "Value for #{k} metadata.", [bin.path, '-e', k, source.path] }
-
-      MultiMarkdownParser.stub_const(:ENV, env) do
-        MultiMarkdownParser.stub_const(:ShellRunner, runner) do
-          MultiMarkdownParser.stub_const(:File, file_lib) do
-            mmd      = MultiMarkdownParser.new
-            metadata = mmd.load_file_metadata(source, *fb_keys)
-            assert_instance_of Hash, metadata
-            assert_equal keys, metadata.keys
-            assert_equal keys.count, metadata.select {|k,v| v == "Value for #{k} metadata." }.count
-          end
-        end
-      end
-
-      env.verify
-      runner.verify
-      file_lib.verify
+    Stubs.with_runner(@runner) do
+      mmd  = MultiMarkdownParser.new
+      data = mmd.load_file_metadata(@source, *fallback)
+      assert_instance_of Hash, data
+      assert_equal       keys, data.keys
+      assert_equal keys.count, data.select {|k,v| v == "Value for #{k} metadata." }.count
     end
+
+    @runner.verify
   end
 
-  # pure stub test for the Markdown conversion MMD command
-  def test_generates_correct_mmd_command_for_conversion
-    source = Struct::File.new('/rand/new/source.md')
+  def test_load_file_metadata_queries_fallback_if_m_unsupported
+    fallback = @meta_keys.sample(3)
+    keys     = @meta_keys
+    bin      = Stubs::Bin.new(@exec.sample.path, true, MultiMarkdownParser::MINIMUM_VERSION)
 
-    out_formats = [:html, :latex, :beamer, :memoir, :odf, :opml]
-    out_formats.each do |format|
-      out_file = Struct::File.new('/rand/new/source.md')
-      env      = MockEnv.new
-      file_lib = MockFile.new(@bins)
+    @runner.expect_shell_query_returning(bin)
+    @runner.expect_version_query_for(bin)
+    fallback.each do |key| @runner.expect_metadata_key_query_for(key, @source, bin, "Value for #{key} metadata.") end
 
-      runner   = MockShellRunner.new(@bins)
-      runner.expect :run_command, '', [@best.path, '-t', String(format), source.path]                      # to stdout
-      runner.expect :run_command, '', [@best.path, '-t', String(format), '-o', out_file.path, source.path] # to file
-
-      MultiMarkdownParser.stub_const(:ENV, env) do
-        MultiMarkdownParser.stub_const(:ShellRunner, runner) do
-          MultiMarkdownParser.stub_const(:File, file_lib) do
-            mmd = MultiMarkdownParser.new
-            mmd.convert_file(source, to_format: format)                        # to stdout
-            mmd.convert_file(source, to_format: format, output_file: out_file) # to file
-          end
-        end
-      end
-
-      env.verify
-      runner.verify
-      file_lib.verify
+    Stubs.with_runner(@runner) do
+      mmd  = MultiMarkdownParser.new
+      data = mmd.load_file_metadata(@source, *fallback)
+      assert_instance_of     Hash, data
+      assert_equal       fallback, data.keys
+      assert_equal fallback.count, data.select {|k,v| v == "Value for #{k} metadata." }.count
     end
+
+    @runner.verify
+  end
+
+  def test_load_file_returns_empty_hash_on_empty_m_list
+    bin = Stubs::Bin.new(@exec.sample.path, true, MultiMarkdownParser::BASELINE_VERSION)
+
+    @runner.expect_shell_query_returning(bin)
+    @runner.expect_version_query_for(bin)
+    @runner.expect_metadata_listing_query_for(@source, bin, "")
+    Stubs.with_runner(@runner) do
+      mmd  = MultiMarkdownParser.new
+      data = mmd.load_file_metadata(@source, @meta_keys.sample(3))
+      assert_instance_of Hash, data
+      assert_empty       data
+    end
+    @runner.verify
+  end
+
+  def test_convert_file_generates_correct_mmd_command
+    output  = @source.sub(/md$/, 'out')
+    formats = [:html, :latex, :beamer, :memoir, :odf, :opml]
+
+    @runner.expect_shell_query_returning(@best)
+    @runner.expect_version_query_for(@best)
+
+    Stubs.with_runner(@runner) do
+      mmd = MultiMarkdownParser.new
+      formats.each do |format|
+        @runner.expect_conversion_command_for(@source, @best, to_format: format)
+        mmd.convert_file(@source, to_format: format)
+
+        @runner.expect_conversion_command_for(@source, @best, to_file: output, to_format: format)
+        mmd.convert_file(@source, to_format: format, output_file: output)
+      end
+    end
+    @runner.verify
   end
 end
