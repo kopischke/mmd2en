821ff6a0e705f11151d98e4085220044341aa75a
Martin Kopischke <martin@kopischke.net>
2014-01-22 00:54:20 +0100

    SemanticVersion: full support for spec 2.0.0
    Make prerelease and build info aware, add version bumping and metadata
    manipulation facilities.

--- lib/semantic_version.rb
+++ lib/semantic_version.rb
@@ -1,29 +1,67 @@
 # encoding: UTF-8
 
-# Quick and dirty semantic versioning class: will recognize (and compare to)
-# anything resembling a semantic version String after conversion (“resembling”
-# because trailing 0s can be omitted, i.e. “4” will be equivalent to “4.0.0”).
-# @note all parts are read-only and build information is dropped
-#   – use a gem (like *Version* or *Versionify*) if you need greater capabilities.
-# @see http://semver.org/ Semantic versioning specification.
+# Semantic Versioning conforming version class.
+#
+# A SemanticVersion object will validate (and compare to) anything resembling
+# a semantic version String after conversion (“resembling” because trailing 0s
+# can be omitted, i.e. “4” will be equivalent to “4.0.0”).
+#
+# @example Creating and comparing semantic versions:
+#   SemanticVersion.new('1.1.2') < 1.2                   # => true
+#   SemanticVersion.new(1.2) == '1.2.0'                  # => true
+#   SemanticVersion.new('2.0+b.212') == '2.0.0'          # => true
+#   SemanticVersion.new('2.0-pre.1') < 2                 # => true
+#   SemanticVersion.new(1) > SemanticVersion.new('2.19') # => false
+#
+# SemanticVersion versions can be bumped in a spec conforming fashion.
+#
+# @example Bumping versions:
+#   SemanticVersion.new('1.1.2').bump.to_full_version         # => "1.1.3"
+#   SemanticVersion.new('1.1.2').bump(:minor).to_full_version # => "1.2.0"
+#   SemanticVersion.new('1.1.2').bump(:major).to_full_version # => "2.0.0"
+#   with_pre_info = SemanticVersion.new('1.0-pre')
+#   with_pre_info.to_full_version                             # => "1.0.0-pre"
+#   with_pre_info.bump(:minor).to_full_version                # => "1.1.0"
+#
+# @see http://semver.org/ Semantic Versioning specification.
+# @note build and prerelease metadata is retained, but not used in comparisons.
+#
 # @author Martin Kopischke
-# @version 1.0.0
+# @version {SemanticVersion::VERSION}
 class SemanticVersion
   include Comparable
 
-  # Generate a SemanticVersion object from anything that can reasonably be cast to a version String.
-  # @param version [String, Float, Integer, SemanticVersion] a version value.
-  # @raise [ArgumentError] if `version` cannot be parsed into a semantic version.
+  # The class version.
+  VERSION          = '1.1.0'
+  # The supported Semantic Version specification version.
+  SPEC_VERSION     = '2.0.0'
+  # The canonical part names of a semantic version.
+  VERSION_PARTS    = [:major, :minor, :patch]
+  # Matches spec conforming metadata tails.
+  METADATA_PATTERN = '([-+])([A-Za-z0-9][-A-Za-z0-9]*(?:\.[A-Za-z0-9][-A-Za-z0-9]*)*)$'
+  # Matches spec conforming version strings.
+  VERSION_PATTERN  = "^([0-9]+(?:\.[0-9]+){0,2})(?:#{METADATA_PATTERN})?$"
+
+  private_constant :METADATA_PATTERN, :VERSION_PATTERN
+
+  # @return [String] the trailing version info part, if any.
+  attr_reader :metadata
+
+  # Create a new SemanticVersion object from version input.
+  # @param version [#to_s] a valid version value after String conversion.
+  # @raise [ArgumentError] if `version` cannot be parsed as a semantic version.
   def initialize(version)
     version  = String(version).strip
-    match    = version.match(/^[0-9]+(?:\.[0-9]+){0,2}((?:[-+]).+)?$/) or
-      raise ArgumentError, "'#{version}' is not a valid semantic version!"
-    parts    = version.chomp(match[1]).split('.')
+    match    = version.match(Regexp.new(VERSION_PATTERN)) or
+      fail ArgumentError, "'#{version}' is not a valid semantic version!"
+    parts    = match[1].split('.')
     @version = {
-      major: Integer(parts[0]),
-      minor: parts[1] ? Integer(parts[1]) : 0,
-      patch: parts[2] ? Integer(parts[2]) : 0
+      VERSION_PARTS[0] => Integer(parts[0]),
+      VERSION_PARTS[1] => parts[1] ? Integer(parts[1]) : 0,
+      VERSION_PARTS[2] => parts[2] ? Integer(parts[2]) : 0
     }
+    @metadata   = String(match[3])
+    @prerelease = match[2] == '-'
   end
 
   # Dynamic accessors for the version parts Hash.
@@ -40,32 +78,105 @@ class SemanticVersion
     @version[symbol] || super
   end
 
-  # All components of the semantic version number.
-  # @return [Array<Integer>] in order of :major, :minor, :patch.
+  # Get a new SemanticVersion object with a version bump.
+  # @note bumping sets all version parts below the bumped one to 0
+  #   and voids all metadata (use {#metadata=} to set metadata info).
+  # @param part [Symbol] the version part to bump.
+  # @return [SemanticVersion] a new SemanticVersion.
+  def bump(part = VERSION_PARTS.last)
+    self.to_version.bump!(part)
+  end
+
+  # Bump the version.
+  # @param (see #bump)
+  # @return [self]
+  def bump!(part = VERSION_PARTS.last)
+    fail ArgumentError, "'#{part}' is not a valid version part!" unless VERSION_PARTS.include?(part)
+    @version[part] += 1
+    VERSION_PARTS.reverse.take_while {|p| p != part }.each do |p| @version[p] = 0 end
+    self.metadata = ''
+    self
+  end
+
+  # Kill prerelease info.
+  # @return [self]
+  def release!
+    self.metadata = '' if prerelease?
+    self
+  end
+
+  # (Re-)set version metadata.
+  # @param metadata [String] the metadata info, including the leading '+' or '-'.
+  # @return [void]
+  # @raise [ArgumentError] if `metadata`is not valid metadata String.
+  def metadata=(metadata)
+    match = metadata.match(Regexp.new(METADATA_PATTERN))
+    if match
+      @metadata   = String(match[2])
+      @prerelease = match[1] == '-'
+    elsif !metadata.empty?
+      fail ArgumentError, "'#{metadata}' is not valid metadata!"
+    else
+      @metadata   = ''
+      @prerelease = false
+    end
+  end
+
+  # Check if this a pre-release version.
+  # @return [Boolean] is this a pre-release?
+  def prerelease?
+    @prerelease == true
+  end
+
+  # All parts the version.
+  # @return [Array<Integer>] in order of {VERSION_PARTS}.
   def to_a
-    @version.values
+    VERSION_PARTS.map {|part| @version[part] }
   end
 
   alias_method :to_ary, :to_a
 
-  # All components of the semantic version number indexed by part name.
-  # @return [Hash<Symbol, Integer>] indexed on :major, :minor, :patch.
+  # All parts of the version, indexed by {VERSION_PARTS} values.
+  # @return [Hash<Symbol, Integer>].
   def to_h
     @version.dup
   end
 
   alias_method :to_hash, :to_h
 
-  # The full semantic version number String matching the version.
-  # @return [String] the canonical string representation.
+  # @return [String] the version String representation, **excluding** metadata.
   def to_s
     self.to_a.join('.')
+  end
+
+  alias_method :to_gem_version, :to_s
+
+  # @return [String] the version String representation, **including** metadata.
+  def to_str
+    "#{self.to_s}#{prerelease? ? '-' : '+' unless @metadata.empty?}#{@metadata}"
+  end
+
+  alias_method :to_full_version, :to_str
+
+  # @return [SemanticVersion] a new version object with the same values.
+  def to_version
+    SemanticVersion.new(self.to_full_version)
+  end
+
+  # @return [String] a human-readable representation of the version object.
+  def inspect
+    @version.inspect
   end
 
   # Comparable base method.
   # @param (see #initialize)
   # @return [-1, 0, 1]
   def <=>(version)
-    self.to_a <=> SemanticVersion.new(version).to_a
+    compare_to = SemanticVersion.new(version)
+    comparison = self.to_a <=> compare_to.to_a
+    case comparison
+    when 0 then (self.prerelease? ? -1 : 0) - (compare_to.prerelease? ? -1 : 0)
+    else comparison
+    end
   end
 end
--- test/semantic_version_test.rb
+++ test/semantic_version_test.rb
@@ -4,39 +4,90 @@ require 'semantic_version'
 
 class TestSemanticVersion < Minitest::Test
   def setup
-    @test_hash = {major: rand(1..9), minor: rand(1..20), patch: rand(1..100)}
-    @test_ary  = @test_hash.values
-    @test_str  = @test_ary.join('.')
-    @test_ver  = SemanticVersion.new("#{@test_str}-alpha4")
+    @hash       = {major: rand(1..9), minor: rand(1..20), patch: rand(1..100)}
+    @array      = @hash.values
+    @string     = @array.join('.')
+    @prerelease = '-alpha.4'
+    @build_info = '+rc.5.21'
+    @version    = SemanticVersion.new("#{@string}")
   end
 
-  def test_parses_a_full_string
-    v = SemanticVersion.new('1.5.3')
-    assert_equal 1, v.major
-    assert_equal 5, v.minor
-    assert_equal 3, v.patch
+  def test_class_exposes_version_parts_contant
+    assert_instance_of Array, SemanticVersion::VERSION_PARTS
+    SemanticVersion::VERSION_PARTS.each do |part| assert_instance_of Symbol, part end
   end
 
-  def test_ignores_build_info
-    v = SemanticVersion.new('4.12.9-alpha')
-    assert_equal 4,  v.major
-    assert_equal 12, v.minor
-    assert_equal 9,  v.patch
+  def test_class_exposes_version_constants
+    [SemanticVersion::VERSION, SemanticVersion::SPEC_VERSION].each do |const|
+      assert_instance_of String, const
+      refute_empty       const
+    end
   end
 
-  def test_parses_a_partial_string
-    v = SemanticVersion.new('2.1')
-    assert_equal 2, v.major
-    assert_equal 1, v.minor
+  def test_exposes_to_version_methods
+    assert_respond_to  @version, :to_version
+    assert_respond_to  @version, :to_full_version
+    assert_respond_to  @version, :to_gem_version
+  end
+
+  def test_exposes_bump_methods
+    assert_respond_to @version, :bump
+    assert_respond_to @version, :bump!
+  end
+
+  def test_exposes_prerelease_test
+    assert_respond_to @version, :prerelease?
+  end
+
+  def test_exposes_metadata_accessor
+    assert_respond_to @version, :metadata
+    assert_respond_to @version, :metadata=
+  end
+
+  def test_new_parses_a_full_string
+    v = SemanticVersion.new(@string)
+    @hash.keys.each do |key| assert_equal @hash[key], v.send(key) end
+    assert_empty v.metadata
+    refute       v.prerelease?
+
+    v = SemanticVersion.new("#{@string}#{@prerelease}")
+    @hash.keys.each do |key| assert_equal @hash[key], v.send(key) end
+    assert_equal @prerelease[1..-1], v.metadata
+    assert       v.prerelease?
+
+    v = SemanticVersion.new("#{@string}#{@build_info}")
+    @hash.keys.each do |key| assert_equal @hash[key], v.send(key) end
+    assert_equal @build_info[1..-1], v.metadata
+    refute       v.prerelease?
+  end
+
+  def test_new_parses_a_partial_string
+    v = SemanticVersion.new([@hash[:major], @hash[:minor]].join('.'))
+    [:major, :minor].each do |key| assert_equal @hash[key], v.send(key) end
     assert_equal 0, v.patch
+    assert_empty v.metadata
+    refute       v.prerelease?
 
-    v = SemanticVersion.new('3')
-    assert_equal 3, v.major
-    assert_equal 0, v.minor
-    assert_equal 0, v.patch
+    v = SemanticVersion.new(String(@hash[:major]))
+    assert_equal @hash[:major], v.major
+    [:minor, :patch].each do |key| assert_equal 0, v.send(key)end
+    assert_empty v.metadata
+    refute       v.prerelease?
+
+    v = SemanticVersion.new("#{@hash[:major]}#{@prerelease}")
+    assert_equal @hash[:major], v.major
+    [:minor, :patch].each do |key| assert_equal 0, v.send(key)end
+    assert_equal @prerelease[1..-1], v.metadata
+    assert       v.prerelease?
+
+    v = SemanticVersion.new("#{@hash[:major]}#{@build_info}")
+    assert_equal @hash[:major], v.major
+    [:minor, :patch].each do |key| assert_equal 0, v.send(key)end
+    assert_equal @build_info[1..-1], v.metadata
+    refute       v.prerelease?
   end
 
-  def test_parses_a_numeric
+  def test_new_parses_a_numeric
     v = SemanticVersion.new(0.7)
     assert_equal 0, v.major
     assert_equal 7, v.minor
@@ -53,42 +104,104 @@ class TestSemanticVersion < Minitest::Test
     assert_equal 0,  v.patch
   end
 
-  def test_converts_to_string_explicitly
-    version = '12.0.3'
-    assert_equal @test_str, @test_ver.to_s
-    assert_equal "Version #{version}.", "Version #{SemanticVersion.new(version)}."
+  def test_converts_to_string
+    assert_equal @string, @version.to_s
+    assert_equal @string, @version.to_gem_version
+    assert_equal "#{@string}#{@info}", @version.to_str
+    assert_equal "#{@string}#{@info}", @version.to_full_version
   end
 
   def test_converts_to_array
-    assert_equal @test_ary, @test_ver.to_a
-    assert_equal @test_ary, @test_ver.to_ary
+    assert_equal @array, @version.to_a
+    assert_equal @array, @version.to_ary
   end
 
   def test_converts_to_hash
-    assert_equal(@test_hash, @test_ver.to_h)
-    assert_equal(@test_hash, @test_ver.to_hash)
+    assert_equal(@hash, @version.to_h)
+    assert_equal(@hash, @version.to_hash)
   end
 
   def test_raises_error_if_passed_invalid_version_representation
-    assert_raises(ArgumentError) { SemanticVersion.new('1.2a')    }
+    assert_raises(ArgumentError) { SemanticVersion.new('1.2.0a')  }
     assert_raises(ArgumentError) { SemanticVersion.new([1, 2, 0]) }
     assert_raises(ArgumentError) { SemanticVersion.new('')        }
     assert_raises(ArgumentError) { SemanticVersion.new(nil)       }
   end
 
   def test_implements_comparable
-    Comparable.instance_methods.each do |method| assert_respond_to @test_ver, method end
+    Comparable.instance_methods.each do |method| assert_respond_to @version, method end
   end
 
   def test_compares_to_other_versions
-    va = SemanticVersion.new('0.3.9-ab')
-    vb = SemanticVersion.new('0.3.9')
-    vc = SemanticVersion.new('0.3.7+build110')
-    assert_equal  0, (va <=> vb)
-    assert_equal  0, (vb <=> va)
-    assert_equal  1, (va <=> vc)
-    assert_equal -1, (vc <=> va)
-    assert_equal  1, (vb <=> vc)
-    assert_equal -1, (vc <=> va)
+    va = SemanticVersion.new(@string)
+    vb = SemanticVersion.new(@string).bump!
+    vc = SemanticVersion.new(@string).bump!(:minor)
+    vd = SemanticVersion.new(@string).bump!(:major)
+    assert_equal(-1, va <=> vb)
+    assert_equal(-1, vb <=> vc)
+    assert_equal(-1, vc <=> vd)
+  end
+
+  def test_compares_to_string_and_numeric
+    base    = "#{@hash[:major]}.#{@hash[:minor]}".gsub(/0$/, '') # trailing zeroes get lost in #to_f
+    version = SemanticVersion.new(base)
+    assert_equal 0, version <=> base
+    assert_equal 0, version <=> base.to_f
+  end
+
+  def test_comparison_minds_prerelease
+    va = SemanticVersion.new("#{@string}#{@prerelease}")
+    vb = SemanticVersion.new(@string)
+    assert_equal(-1, va <=> vb)
+  end
+
+  def test_comparison_ignores_build_metadata
+    va = SemanticVersion.new(@string)
+    vb = SemanticVersion.new("#{@string}#{@build_info}")
+    assert_equal 0, (va <=> vb)
+  end
+
+  def test_to_version_returns_new_version_object
+    assert_instance_of SemanticVersion, @version.to_version
+    refute_equal       @version.to_version.bump!, @version
+  end
+
+  def test_bump_bumps_version_parts
+    assert_equal @version.bump, @version.bump(:patch)
+    @hash.keys.each do |part|
+      bumped_hash = @hash.dup
+      bumped_hash[part]  += 1
+      bumped_hash[:patch] = 0 if part == :minor || part == :major
+      bumped_hash[:minor] = 0 if part == :major
+
+      assert_equal bumped_hash, @version.bump(part).to_hash
+      assert_equal @hash,       @version.to_hash # unmodified object
+
+      bumped = SemanticVersion.new(@string).bump!(part)
+      assert_equal bumped_hash, bumped.to_hash
+      assert_empty              bumped.metadata
+    end
+  end
+
+  def test_bump_voids_metadata
+    assert_empty SemanticVersion.new("#{@string}#{@prerelease}").bump!.metadata
+  end
+
+  def test_release_voids_prerelease_info
+    @version.metadata = @prerelease
+    @version.release!
+    assert_empty @version.metadata
+    refute       @version.prerelease?
+
+    @version.metadata = @build_info
+    @version.release!
+    refute_empty @version.metadata
+    refute       @version.prerelease?
+  end
+
+  def test_metadata_setter_fails_if_metadata_invalid
+    @version.metadata = @build_info
+    assert_equal @build_info[1..-1], @version.metadata
+    assert_raises (ArgumentError) { @version.metadata = 'foobar' }
   end
 end
