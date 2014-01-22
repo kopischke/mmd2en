20bd89eda910643399f44c20d6706cd1fa5ac039
Martin Kopischke <martin@kopischke.net>
2014-01-22 01:55:28 +0100

    Core Extensions: File#incremental_write
    Make incremental writing as convenient as `file.write(file.read)`.

--- lib/core_ext/file.rb
+++ lib/core_ext/file.rb
@@ -4,5 +4,30 @@ module CoreExtensions
   # @author Martin Kopischke
   # @version {CoreExtensions::VERSION}
   class ::File
+    # Write content to a file line by line.
+    #
+    # @!macro [new] file.incremental_write
+    #   Opens a new handle to the file in 'wb' mode, writes the content
+    #   Line by line, then closes the handle. `content` that does not respond to
+    #   #each_line will be converted to a String.
+    # @param file [File, String] the file to write to.
+    # @param content [#each_line, #to_s] the content to write.
+    # @return [Integer] the number of bytes written.
+    # @return [nil] if no encoding could be guessed or `file` is not an existing file.
+    def self.incremental_write(file, content)
+      content = String(content) unless content.respond_to?(:each_line)
+      File.open(file, 'wb') do |f|
+        content.each_line.with_object(0) do |line, bytes| bytes += f.write(line) end
+      end
+    end
+
+    # Write content to the file line by line.
+    #
+    # @!macro file.incremental_write
+    # @param (see self.incremental_write)
+    # @return (see self.incremental_write)
+    def incremental_write(content)
+      File.incremental_write(self, content)
+    end
   end
 end
--- test/core_ext_test/file.rb
+++ test/core_ext_test/file.rb
@@ -1,5 +1,47 @@
 # encoding: UTF-8
 require 'core_ext/file'
+require 'tempfile'
+
+module ContentHandling
+  def reference_content(content)
+    reference = case
+      when content.is_a?(File)   then File.read(content)
+      when content.is_a?(String) then content
+      else String(content)
+    end
+  end
+end
 
 class TestFile < Minitest::Test
+  include ContentHandling
+
+  def setup
+    @file    = File.new(__FILE__)
+    @tmpfile = Tempfile.new('TEST')
+    @content = [@file, 'Some string', :not_a_string]
+  end
+
+  def teardown
+    @file.close
+    @tmpfile.close!
+  end
+
+  def test_exposes_incremental_write_methods
+    assert_respond_to File,  :incremental_write
+    assert_respond_to @file, :incremental_write
+  end
+
+  def test_class_incremental_write_writes_all_content
+    @content.each do |content|
+      File.incremental_write(@tmpfile, content)
+      assert_equal File.read(@tmpfile), reference_content(content)
+    end
+  end
+
+  def test_instance_incremental_write_writes_all_content
+    @content.each do |content|
+      File.new(@tmpfile).incremental_write(content)
+      assert_equal File.read(@tmpfile), reference_content(content)
+    end
+  end
 end
